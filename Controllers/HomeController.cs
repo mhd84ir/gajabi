5f59b15a21664bbb8841f726b56773a5323282da
mhd84ir <mhd84.ir@gmail.com>
2023-08-09 13:27:37 +0330

    18mordad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,21 @@ public class HomeController : Controller
         return View();
     }
 
+        public IActionResult singin()
+    {
+        return View();
+    }
+
+    public IActionResult singup()
+    {
+        return View();
+    }
+
+        public IActionResult cartdesktop()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
