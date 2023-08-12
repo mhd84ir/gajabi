6008eb606c0bc27d87b9ab33fc26309e28a45fb0
mhd84ir <mhd84.ir@gmail.com>
2023-08-13 01:09:21 +0330

    22mordad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -52,4 +52,10 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult test()
+    {
+        return View();
+    }
+
 }
